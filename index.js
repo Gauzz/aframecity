816c268883743f83671aec7f74634c2013dfb0f1
Gaurav Rozatkar <gauravrozatkar1@gmail.com>
2019-11-11 12:09:26 +0530

    new city

--- index.js
+++
@@ -1,6 +0,0 @@
-require('aframe-gridhelper-component');
-require('aframe-animation-component');
-require('./lib/action-controls.js');
-require('./lib/builder-controls.js');
-require('./lib/ground.js');
-require('./lib/skyGradient.js');
